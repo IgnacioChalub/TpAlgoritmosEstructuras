ccc23e075682d69cca71e4436b90b841e1b49600
Matias Coulougian <matias.coulougian@ing.austral.edu.ar>
2021-03-16 17:32:27 -0300

    Test junit avance

--- src/MiniBlast/Blast.java
+++ src/MiniBlast/Blast.java
@@ -1,13 +1,22 @@
 package MiniBlast;
 
 public class Blast {
-    public static <T> double  BlastComparison(Comparable<T> referenceSequence[], Comparable<T> querySequence[]) {
+    public static double  BlastComparison(char[] referenceSequence, char[] querySequence) {
+        int coincidences = 0; //Counts the elements that match
+        for (int i = 0; i < referenceSequence.length; i++) {
+            if(referenceSequence[i] == querySequence[i]){
+                coincidences++;
+            }
+        }
+        return ((double)coincidences/referenceSequence.length) * 100;
+    }
+    public static double  BlastComparisonPonderation(char[] referenceSequence, char[] querySequence) {
         int coincidences = 0; //Counts the elements that match
         int changesRequired = 0; //Counts the ammounts of blocks of mismatches.
         boolean lastElementWasEqual = false;
 
         for (int i = 0; i < referenceSequence.length; i++) {
-            if (referenceSequence[i].compareTo((T) querySequence[i]) == 0) {
+            if (referenceSequence[i] == querySequence[i]){
                 coincidences++;
                 lastElementWasEqual = true;
             } else if (lastElementWasEqual) {
@@ -15,16 +24,20 @@ public class Blast {
                 lastElementWasEqual = false;
             }
         }
-        return ((double)coincidences/referenceSequence.length) * 100;
+        return (((double)coincidences/referenceSequence.length) * 0.8 + ((double)changesRequired/referenceSequence.length) * 0.2) * 100;
     }
 
     public static void main(String[] args) {
-        String[] ref = {"baa", "a", "asd","qqqq","dasaf","ssg"};
-        String[] qry = {"aaa", "a", "asd","qqqq","das","sgsfg"};
-        String[]ref2 = {"baa", "a", "asds","qqqq","sf","sgsfg"};
+        char[] qry = {'a', 'g', 'c','t','t','t'};
+        char[] ref = {'u', 'g', 'g','t','a','t'};
+        char[]ref2 = {'c', 'a', 'g','t','t','t'};
 
-        System.out.println("Coincidence Percentaje 1 : "+BlastComparison(ref,qry) + "%");
-        System.out.println("Coincidence Percentaje 2: "+BlastComparison(ref2,qry) + "%");
+
+        System.out.println("Coincidence Percentage 1 : "+BlastComparison(ref,qry) + "%");
+        System.out.println("Coincidence Percentage 2: "+BlastComparison(ref2,qry) + "%");
+
+        System.out.println("Coincidence Percentage 1 : "+BlastComparisonPonderation(ref,qry) + "%");
+        System.out.println("Coincidence Percentage 2 : "+BlastComparisonPonderation(ref,qry) + "%");
 
     }
 }
